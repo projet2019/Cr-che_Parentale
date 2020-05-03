333ae7455c861e087bd175a1a608ae220ed399e5
joel yepgang <joelyepgang2019@yahoo.com>
2020-05-03 20:16:03 +0200

    add models and class

--- creche/creche/settings.py
+++ creche/creche/settings.py
@@ -15,17 +15,44 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+COREAPP_ROOT = os.path.join(BASE_DIR, 'coreapp')
+## custom settings 
+
+APPLICATION_SETTINGS = {
+    'common': {
+        'smtp_host': 'localhost',
+        'admin_to_email': 'creche@localhost',
+        'admin_from_email': 'admin@localhost',
+        'event_notification_from_email': 'noreply@localhost',
+        'local_repo': 'data-repo'
+    },
+    'development': {
+
+    },
+    'UNKNOWN_RECORD_EVENT_TYPE': 1,
+    'MISSING_FILE_EVENT_TYPE': 2,
+    'UNKNOWN_COLUMN_EVENT_TYPE': 3,
+    'UNKNOWN_ERROR_EVENT_TYPE': 4,
+    'TEST_EMAIL': 5,
+    'ENGLISH_EMAIL': 6,
+    'FRENCH_EMAIL': 7,
+    'COREAPP_HOME': COREAPP_ROOT,
+    'EVENT_NOTIFICATION_TPL': '/public/templates/email/significant-event-notification-en.html',
+    'EMAIL_HOME': '/data-repo/files/emails', 
+    'ROOT_URL': 'http://localhost/creche'
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o0fc_te1(v-5_b^+en3yhkn9^jh^so^3(%9tjc9_xarbk@k#$k'
+SECRET_KEY = 'x)1il2bug2b8o-=((ifhit^-#k&5w!0=rpu%_6ar1j5akjg*3!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
 
 
 # Application definition
@@ -37,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coreapp.apps.CoreappConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,14 +75,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
+CSRF_USE_SESSIONS = True
 ROOT_URLCONF = 'creche.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [APPLICATION_SETTINGS['COREAPP_HOME'] + '/public/templates/html/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +105,13 @@ WSGI_APPLICATION = 'creche.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'creche',
+        'USER': 'creche', # Not used with sqlite3.
+        'PASSWORD':  'cReche@123!', # Not used with sqlite3.
+        'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+	"CONN_MAX_AGE": 0,
     }
 }
 
@@ -117,4 +152,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    APPLICATION_SETTINGS['COREAPP_HOME'] + '/public/'
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
