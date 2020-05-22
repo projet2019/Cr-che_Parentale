8cd483d5ea075a9542876ae97a4803c39006f455
satsisat <satsisat@yahoo.fr>
2020-05-22 15:26:12 +0200

    update

--- creche/coreapp/controller/system_user_controller.py
+++ creche/coreapp/controller/system_user_controller.py
@@ -4,7 +4,7 @@
 
 ##
 ##
-## @author   Joel
+## @author Nadia
 ## nadia@gmail.com/joel@gmail.com
 ##
 
@@ -15,138 +15,145 @@ from coreapp.util.app_util import json_encode
 from coreapp.util.export_util import ExportUtil
 from django.http import HttpResponse
 
+
 class SystemUserController(BaseController):
     pass
 
+
 def loginAudit(request):
-    #TO-DO check if this user has a valid session
+    # TO-DO check if this user has a valid session
     controller = SystemUserController()
-    
-    try:
-Nadia   service = SystemUserService()
 
-Nadia   result = service.loginAudit(request.POST)
+    try:
+        service = SystemUserService()
+
+        result = service.loginAudit(request.POST)
 
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia   content_type="application/json")
-NadiaNadiaNadiaNadia   
+                        content_type="application/json")
+
+
 def list(request):
-    #TO-DO check if this user has a valid session
+    # TO-DO check if this user has a valid session
     controller = SystemUserController()
-    
-    try:
-Nadia   service = SystemUserService()
 
-Nadia   result = service.list(request.POST)
+    try:
+        service = SystemUserService()
+
+        result = service.list(request.POST)
 
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia   content_type="application/json")
+                        content_type="application/json")
+
 
 def listExport(request):
-    #TO-DO check if this user has a valid session
+    # TO-DO check if this user has a valid session
     controller = SystemUserController()
 
     try:
-Nadia   service = SystemUserService()
+        service = SystemUserService()
 
-Nadia   headers, records = service.listExport(request.GET)
+        headers, records = service.listExport(request.GET)
 
-Nadia   return ExportUtil.export(headers, records, request.GET['exportType'])
+        return ExportUtil.export(headers, records, request.GET['exportType'])
 
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia   content_type="application/json")
+                        content_type="application/json")
+
 
 def list_principals(request):
     # TO-DO check if this user has a valid session
     controller = SystemUserController()
 
     try:
-Nadia   service = PrincipalService()
+        service = PrincipalService()
 
-Nadia   result = service.list(request.POST)
+        result = service.list(request.POST)
 
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia    content_type="application/json")
+                        content_type="application/json")
+
 
 def userLoggedOn(request):
-    #TO-DO check if this user has a valid session
+    # TO-DO check if this user has a valid session
     controller = SystemUserController()
 
     try:
-Nadia   service = SystemUserService()
+        service = SystemUserService()
 
-Nadia   user = service.userLoggedOn(request.session, request.POST)
-Nadia   result = {'success': True, 'data' : user}
+        user = service.userLoggedOn(request.session, request.POST)
+        result = {'success': True, 'data': user}
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia   content_type="application/json")
+                        content_type="application/json")
 
 
 def saveUser(request):
-    #TO-DO check if this user has a valid session
+    # TO-DO check if this user has a valid session
     controller = SystemUserController()
 
     try:
-Nadia   service = SystemUserService()
+        service = SystemUserService()
 
-Nadia   service.save(request.POST)
-NadiaNadia  
-Nadia   result = {'success': True, 'message' : 'User details successfully updated. You can now login into the platform.'}
+        service.save(request.POST)
+
+        result = {'success': True, 'message': 'User details successfully updated. You can now login into the platform.'}
 
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia   content_type="application/json")
+                        content_type="application/json")
 
 
 def passwordForget(request):
-    #TO-DO check if this user has a valid session
+    # TO-DO check if this user has a valid session
     controller = SystemUserController()
 
     try:
-Nadia   service = SystemUserService()
+        service = SystemUserService()
 
-Nadia   service.passwordForget(request.POST)
-NadiaNadia  
-Nadia   result = {'success': True, 'message' : 'The password reset instructions have been sent to your email address.'}
+        service.passwordForget(request.POST)
+
+        result = {'success': True, 'message': 'The password reset instructions have been sent to your email address.'}
 
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia   content_type="application/json")
+                        content_type="application/json")
+
 
 def passwordChange(request):
-    #TO-DO check if this user has a valid session
+    # TO-DO check if this user has a valid session
     controller = SystemUserController()
 
     try:
-Nadia   service = SystemUserService()
+        service = SystemUserService()
 
-Nadia   service.passwordChange(request.POST)
-NadiaNadia  
-Nadia   result = {'success': True, 'message' : 'You have changed your password successfully.'}
+        service.passwordChange(request.POST)
+
+        result = {'success': True, 'message': 'You have changed your password successfully.'}
 
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia   content_type="application/json")
+                        content_type="application/json")
 
 
 def savePrincipal(request):
@@ -154,15 +161,14 @@ def savePrincipal(request):
     controller = SystemUserController()
 
     try:
-Nadia   service = PrincipalService()
-Nadia   service.save_principal(request.POST)
-Nadia   result = {'success': True, 'message': 'Principal details successfully saved. You can now view it.'}
+        service = PrincipalService()
+        service.save_principal(request.POST)
+        result = {'success': True, 'message': 'Principal details successfully saved. You can now view it.'}
 
     except Exception as e:
-Nadia   result = controller.handleException(e)
+        result = controller.handleException(e)
 
     return HttpResponse(json_encode(result),
-NadiaNadiaNadiaNadia    content_type="application/json")
+                        content_type="application/json")
 
 
-    
