8c684eac5aaf81224ed3257c1f3e03ef9d26b2a0
satsisat <satsisat@yahoo.fr>
2020-08-19 19:32:25 +0200

    update error OrderedDict

--- creche/coreapp/tests.py
+++ creche/coreapp/tests.py
@@ -1,16 +1,1 @@
 from django.test import TestCase
-from coreapp.service.principal_service import PrincipalService
-# Create your tests here.
-class PrincipalServiceTestCase(TestCase):
-    principal = PrincipalService()
-    def test_save_principal(self):
-        """Testing principal saving process"""
-        nadia = {
-			'principal_names': "Nadia Mb.",
-			'telephone': '0778856565',
-			'id_number': '5',
-			'role': 'Directrice',
-			'full_address' : 'Louvain 789ST',
-			'email': '',
-			};
-        self.assertEqual(self.principal.save_principal(nadia),True);
